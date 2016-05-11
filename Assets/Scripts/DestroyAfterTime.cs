8cfad6ece2012e5afa2c5b53710e868e97140baf
IvanDonat <ivandonat@gmail.com>
2016-05-11 22:35:25 +0200

    Fixed indentation

--- Assets/Scripts/DestroyAfterTime.cs
+++ Assets/Scripts/DestroyAfterTime.cs
@@ -6,7 +6,7 @@ public class DestroyAfterTime : MonoBehaviour {
     private float spawnTime;
 
 	void Start () {
-        spawnTime = Time.time;
+            spawnTime = Time.time;
 	}
 	
 	void Update () {
