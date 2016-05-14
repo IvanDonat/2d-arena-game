3492f72287c3047ba2c24a36ee217ede0c234266
Ivan Donat Pupovac <ivandonat@gmail.com>
2016-05-14 16:37:24 +0200

    Animated Phaseball

---
+++ Assets/SpriteAnimation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteAnimation : MonoBehaviour {
+
+    public Sprite[] sprites;
+    private int currIndex = 0;
+    private SpriteRenderer spriteRenderer;
+
+    public float swapInterval = 0.2f;
+    private float lastTimeSwapped;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+	void Update () {
+        if (Time.time - lastTimeSwapped >= swapInterval)
+        {
+            currIndex++;
+            if (currIndex >= sprites.Length)
+                currIndex = 0;
+            
+            spriteRenderer.sprite = sprites[currIndex];
+            lastTimeSwapped = Time.time;
+        }
+	}
+}
