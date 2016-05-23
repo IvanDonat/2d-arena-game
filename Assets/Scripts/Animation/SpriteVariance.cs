5d2ae137ba995ac4888f5009244c901a3327ee71
Ivan Donat Pupovac <ivandonat@gmail.com>
2016-05-23 23:17:08 +0200

    Reorganized Scripts

---
+++ Assets/Scripts/Animation/SpriteVariance.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteVariance : MonoBehaviour
+{
+
+    public string spritePath;
+    public int gridWidth;
+    public int gridHeight;
+
+
+    void Start()
+    {
+        Sprite[] sprites = Resources.LoadAll<Sprite>(spritePath);
+        GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, gridWidth * gridHeight)];
+
+        transform.localScale = new Vector3(0.8f, 0.8f, 1);
+    }
+}
