0d4e7b969d0eb21d16028b8df908de18b8736cf1
Ivan Donat Pupovac <ivandonat@gmail.com>
2016-06-20 19:31:30 +0200

    Improved the playback of background music

---
+++ Assets/Scripts/Misc/MusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+    void Start()
+    {
+        gameObject.tag = "MusicPlayer";
+        GameObject[] musicPlayers = GameObject.FindGameObjectsWithTag("MusicPlayer");
+
+        if (musicPlayers.Length > 1)
+        {
+            foreach (GameObject player in musicPlayers)
+            {
+                if (player != gameObject)
+                {
+                    if (player.GetComponent<AudioSource>().clip == GetComponent<AudioSource>().clip)
+                    {
+                        Destroy(gameObject);
+                    }
+                    else
+                    {
+                        Destroy(player);
+                    }
+                }
+            }
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
